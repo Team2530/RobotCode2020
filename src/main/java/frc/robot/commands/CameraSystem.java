98bc43a1b6025b77c76188c3944e9b5d50d514dd
Will S <spelled.with.k@gmail.com>
2020-01-14 20:45:04 -0600

    Camera System
    Initialised Camera Command. needs to be implem ented into RobotContainer (or another subsystem).

---
+++ src/main/java/frc/robot/commands/CameraSystem.java
@@ -0,0 +1,50 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+/**
+ * Add your docs here.
+ */
+public class CameraSystem extends CommandBase {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+  public CameraSystem() {
+
+// documentation website
+// https://readthedocs.org/projects/limelight/downloads/pdf/latest/
+
+
+NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+NetworkTableEntry tx = table.getEntry("tx");
+NetworkTableEntry ty = table.getEntry("ty");
+NetworkTableEntry ta = table.getEntry("ta");
+
+//read values periodically
+double x = tx.getDouble(0.0);
+double y = ty.getDouble(0.0);
+double area = ta.getDouble(0.0);
+
+//post to smart dashboard periodically
+SmartDashboard.putNumber("LimelightX", x);
+SmartDashboard.putNumber("LimelightY", y);
+SmartDashboard.putNumber("LimelightArea", area);
+  }
+}
+//  @Override
+ // public void periodic() {
+    
+ // }
