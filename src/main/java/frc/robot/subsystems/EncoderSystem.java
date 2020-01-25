8452b46f20b463daf7778143f66a5b7698d01725
Will S <spelled.with.k@gmail.com>
2020-01-25 14:52:03 -0600

    Got Tic calculations. I guess.
    Still needs to have the encoder initialised in OI and needs to have the encoder litterally implemented (physically) onto the motor thing. needs a get tick command? or maybe a more accurate one. basic calculations made.

--- src/main/java/frc/robot/subsystems/EncoderSystem.java
+++ src/main/java/frc/robot/subsystems/EncoderSystem.java
@@ -13,9 +13,35 @@ public class EncoderSystem extends SubsystemBase {
   /**
    * Creates a new ExampleSubsystem.
    */
+  private final double shaftDiameter = 0.5; //driver
+  private final double wheelDiameter = 6; //driven
+  private final double driverC = shaftDiameter * Math.PI;
+  private final double drivenC = wheelDiameter * Math.PI;
+  private double tics;
+  private final double gearRatio = drivenC / driverC;
+  private double distance1;
+  
   public EncoderSystem() {
     //initialise encoder
   }
+
+
+  public double calculateTics(int distance) {
+    tics = 360 / driverC * distance;
+    return tics;
+  }
+  
+  //calculateDistance(calculateTics(desiredDistance));
+  public double calculateDistance(int tics) {
+    distance1 = tics/ 360 * driverC;
+    return distance1 * gearRatio;
+  }
+  
+//Encoder ticks = (360 / circumference) * Distance to travel
+//Distance traveled = (Encoder ticks / 360) * circumference
+//6 inch wheels
+//1/2 inch shaft thing (connected to the wheel)
+
 
   @Override
   public void periodic() {
