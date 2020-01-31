269e43084aa6b458a1fca0b0070f15cb20911e72
Lukas Singer <spelled.with.k@gmail.com>
2020-01-30 20:38:53 -0600

    Set up Pixy API and added code to fetch data

--- src/main/java/frc/robot/subsystems/pixy.java
+++ src/main/java/frc/robot/subsystems/pixy.java
@@ -1,26 +1,75 @@
-/*All pixy code*/
-
 package frc.robot.subsystems;
 
+import java.util.ArrayList;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import io.github.pseudoresonance.pixy2api.Pixy2;
+import io.github.pseudoresonance.pixy2api.Pixy2CCC;
+import io.github.pseudoresonance.pixy2api.Pixy2CCC.Block;
+import io.github.pseudoresonance.pixy2api.links.SPILink;
 
 public class pixy extends SubsystemBase {
   private DriveTrain driveTrain;
   private Shooter shooter;
   private Elevator elevator;
-  public ballSensor() {
+  private Pixy2 pixy = Pixy2.createInstance(new SPILink());
 
+  // ints for ball data
+  Block loadedBlock;
+  int ballWidth;
+  int ballHeight;
+  int ballX;
+  int ballY;
+  int ballAngle;
+  int ballAge;
+
+  public pixy() {
+    // Initializes the Pixy
+    pixy.init();
   }
+
+  public Block getBiggestBlock() {
+		// Finds blocks that match signature 1,
+    // finds the largest of them,
+    // then returns it in the original data-retrievable format
+    int blockCount = pixy.getCCC().getBlocks(false, Pixy2CCC.CCC_SIG1, 25 /* limit on blocks to return */);
+    
+		if (blockCount <= 0) {
+			return null; // If blocks were not found, stop processing
+    }
+    
+    ArrayList<Block> blocks = pixy.getCCC().getBlocks(); // Gets a list of all blocks found by the Pixy2
+    
+		Block largestBlock = null;
+		for (Block block : blocks) { // Loops through all blocks and finds the widest one
+			if (largestBlock == null) {
+				largestBlock = block;
+			} else if (block.getWidth() > largestBlock.getWidth()) {
+				largestBlock = block;
+			}
+		}
+		return largestBlock;
+	}
 
   @Override
   public void periodic() {
-    if (//button pressed
-    ){
+
+    // Gets Pixy data and stores in variables
+    loadedBlock = getBiggestBlock();
+    ballWidth = loadedBlock.getWidth();
+    ballHeight = loadedBlock.getHeight();
+    ballX = loadedBlock.getX();
+    ballY = loadedBlock.getY();
+    ballAngle = loadedBlock.getAngle();
+    ballAge = loadedBlock.getAge();
+    
+    // Uses the Pixy data to do things
+    /* if (  <button pressed>  ) {
         this.driveTrain = driveTrain;
         this.shooter = shooter;
         this.elevator = elevator;
         //take over drivetrain, elevator, and shooter
-        //stuff when we know what info the pixy gives us
-    }
+        //stuff using above variables
+      } */
   }
 }
