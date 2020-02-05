1bc0c1c6ca3ef8df6fb854c6563b8e249d3db845
Lukas Singer <spelled.with.k@gmail.com>
2020-02-04 20:52:32 -0600

    Uploaded code that runs on the Arduino

---
+++ Arduino Code/RoboRIO_Serial_Response.ino
@@ -0,0 +1,17 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(Serial.available()) {
+    byte value = Serial.read();
+
+    if(value == 0x12) {
+      Serial.println("Arduino says: 'Received code 0x12.'");
+    }
+  }
+
+  delay(50);
+}
