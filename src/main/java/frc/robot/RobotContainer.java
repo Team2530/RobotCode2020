ce8df0f38119b344f57f73e50d79f1229c700802
Peter Marshall <pwmarshall@gmail.com>
2020-02-06 20:15:15 -0600

    Cleaned up Robot Container and started Auto

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,13 +10,8 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.DualLargeJoystickDrive;
-import frc.robot.commands.EncoderTest;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.commands.LargeJoystickDrive;
-import frc.robot.commands.LineUp;
-import frc.robot.commands.TestPixy;
-import frc.robot.commands.ToggleLimeLightLED;
+import frc.robot.commands.*;
+import frc.robot.commands.autonomousCommands.*;
 // import frc.robot.commands.SmallJoystickElevator;
 // import frc.robot.commands.XboxJoystickElevator;
 import frc.robot.subsystems.DriveTrain;
@@ -36,26 +31,37 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
+
+  // -------------------- Subsystems -------------------- \\
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
   private final DriveTrain m_driveTrain = new DriveTrain();
   private final Elevator elevatorSub = new Elevator();
   private final LimeLight limeLightSub = new LimeLight();
   private final Pixy m_pixy = new Pixy();
 
+  // -------------------- Joysticks and Buttons -------------------- \\
+  //Joysticks
   final Joystick stick1 = new Joystick(1); // Creates a joystick on port 1
-  final Joystick stick2 = new Joystick(2); // Creates a joystick on port 1
-  private final JoystickButton Button1 = new JoystickButton(stick1, 1); // Creates a new JoystickButton object for
-                                                                        // button 1 on stick
+  final Joystick stick2 = new Joystick(2); // Creates a joystick on port 2
+
+  //Joystick buttons
+  private final JoystickButton Button1 = new JoystickButton(stick1, 1); // Creates a new button for button 1 on stick1
   private final JoystickButton Button3 = new JoystickButton(stick1, 3);
   private final JoystickButton Button4 = new JoystickButton(stick1, 4);
+
+  //Xbox Controller
   final XboxController xbox = new XboxController(0);
 
+  //Xbox buttons
+  private final JoystickButton XboxButton1 = new JoystickButton(xbox, 1);
+
+  // -------------------- Autonomous Commands -------------------- \\
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
-  
-  // private final XboxJoystickElevator elevatorCommand = new
-  // XboxJoystickElevator(elevatorSub, xbox);
-  // private final SmallJoystickElevator elevatorCommand = new
-  // SmallJoystickElevator(elevatorSub, stick1);
+  private final DelayTest delayCommand = new DelayTest(1, m_autoCommand);
+
+  // -------------------- Telop Commands -------------------- \\
+  // private final XboxJoystickElevator elevatorCommand = new XboxJoystickElevator(elevatorSub, xbox);
+  // private final SmallJoystickElevator elevatorCommand = new SmallJoystickElevator(elevatorSub, stick1);
   // private final EncoderTest m_telopCommand = new EncoderTest(m_driveTrain);
   private final LineUp lineUp = new LineUp(m_driveTrain, limeLightSub, elevatorSub);
   private final TestPixy pixy = new TestPixy(m_pixy);
