203fd5365362975bd6cd1458b7d180ec91e75412
Lukas Singer <spelled.with.k@gmail.com>
2020-03-05 18:51:24 -0600

    Put LineUp in depreciated folder
    (replaced by LineUpRobot)

--- src/main/java/frc/robot/commands/depreciated/LineUp.java
+++ src/main/java/frc/robot/commands/depreciated/LineUp.java
@@ -5,7 +5,7 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package frc.robot.commands;
+package frc.robot.commands; // if in depreciated folder, ignore this error
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
